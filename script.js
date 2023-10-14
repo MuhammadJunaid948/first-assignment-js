c052cc7732a825ea87ef7356404397461e4f0bf8
MuhammadJunaid948 <mjunaidyousafzai948@gmail.com>
2023-10-14 12:09:30 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+
+// // ****************Chapter-1*********************
+
+// // *******************Alerts*************
+// alert("Error! Please Enter the Valid Passowrd");
+// alert("Welcome To The SMIT.\n Happy Coding");
+// alert("Welcome To SMIT");
+// alert("Happy Coding!");
+// console.log("Hello... I can run JavaScript Through my Browser's console");
+
+
+
+// let username;
+// let myName = "Your Full Name";
+// let message;
+// message = "Hello World";
+// alert(message);
+
+
+
+// let studentName = "Junaid";
+// let studentAge = 20;
+// let studentAddress = "123 fake st";
+// let studentGrade = "A+";
+// alert ("Student Name " +  studentName);
+// alert ("Student Age " +  studentAge);
+// alert ("Student Address" +  studentAddress);
+// alert ("Student Grade" +  studentGrade);
+
+// // *******************************Chapter-3*********************************
+// let variable1 = ("PIZZA");
+// let variable2 = ("PIZZ");
+// let variable3 = ("PIZ");
+// let variable4 = ("PI");
+// let variable5 = ("P");
+// alert("variable1 \n variable2 \n variable3 \n variable4 \n variable5");
+
+
+// alert("My Email Address in Junaid_shrood948@gmail.com")
+// var book = "A smarter way to learn Javascript";
+// alert(book);
+
+// document.write("Yah! I can write Html content through Java script ");
+
+
+// let value1 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+// alert(value1);
+
+
+// // ******************************Chapter-3**************************
+
+//     let age = 20;
+//     alert(age);
+
+
+// let visitTime = "you have visited 14 times ";
+// alert(visitTime);
+
+
+// let birthYear = 2003;
+
+// document.write("my birth year is " + birthYear );
+// document.write(" Datatype of my variable is Number" );
+
+
+// let visitorName = "John";
+// let productName = "T-Shirt";
+// let quantity = "5";
+
+// document.write(visitorName +  " ordered" + quantity + productName + "\n on XYZ Clothing Store" );
+
+
+// // *****************Chapeter 4**************************
+
+// document.write("<h1>Rules for naming JS Variables</h1>");
+// document.write("<h3>Variable names can only contain number,$ _. For example $my_1stVariable </h3>");
+// document.write("<h3>\nVariables must begin with a ______, ______ or ______ For example $name, _name or name</h3>");
+// document.write("<h3>Variable names are case sensitive</h3>");
+// document.write("<h3>Variable names should not be JS Keywords</h3>");
+
+
+
+
+
+
+
+
+
+
+
